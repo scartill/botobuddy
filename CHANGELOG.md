70e937adf8f8cfdbb32ed03a6511b5dae93aa991
Boris Resnick <boris.resnick@gmail.com>
2025-10-25 10:51:40 +0300

    feat(awslambda): add get_this_url function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.5.0
+
+- Added `botobuddy.awslambda.get_this_url` function
+
 # 0.4.0
 
 - Added `botobuddy.awslambda.response` function
--- src/botobuddy/awslambda.py
+++ src/botobuddy/awslambda.py
@@ -4,6 +4,9 @@ from decimal import Decimal
 
 
 class DynamoFriendlyEncoder(json.JSONEncoder):
+    """
+    A JSON encoder that converts Decimal objects to integers.
+    """
     def default(self, obj):
         if isinstance(obj, Decimal):
             return int(obj)
@@ -13,6 +16,17 @@ class DynamoFriendlyEncoder(json.JSONEncoder):
 
 
 def response(data_or_error=None, rc=200):
+    """
+    Returns a response object for the current request.
+    Parameters:
+    - data_or_error: The data to return in the response.
+    - rc: The status code to return in the response.
+    Returns:
+    - A response object with the following properties:
+    - statusCode: The status code to return in the response.
+    - headers: The headers to return in the response.
+    - body: The body to return in the response.
+    """
     if rc != 200:
         payload = {
             'IsSuccessful': False,
@@ -36,6 +50,17 @@ def response(data_or_error=None, rc=200):
 
 
 def request_params(event):
+    """
+    Returns the method and parameters of the current request.
+    Example: ('GET', {'path': 'example'})
+    Example: ('POST', {'body': {'key': 'value'}})
+    Example: ('PUT', {'body': {'key': 'value'}})
+    Example: ('DELETE', {'path': 'example'})
+    Example: ('OPTIONS', {})
+    Example: ('PATCH', {'body': {'key': 'value'}})
+    Example: ('HEAD', {})
+    Example: ('TRACE', {})
+    """
     params = dict()
     method = event['httpMethod']
 
@@ -56,3 +81,14 @@ def request_params(event):
         params.update(json.loads(event['body']))
 
     return (method, params)
+
+
+def get_this_url(event):
+    """
+    Returns the full URL of the current request.
+    Example: https://example.com/path
+    """
+    requestContext = event['requestContext']
+    domainName = requestContext['domainName']
+    path = requestContext['path']
+    return f'https://{domainName}{path}'
